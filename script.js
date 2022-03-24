8c79decec111f56ef0474ecbf81c908b5fea2b07
volk <velvel2k@gmail.com>
2022-03-24 18:50:46 +0200

    initial

---
+++ script.js
@@ -0,0 +1,90 @@
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+const display = document.querySelector('h1');
+
+
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber,
+};
+
+
+// variables
+let firstValue = 0;
+let operator = '';
+let awaitsInput = false;
+
+
+
+
+
+
+// displaying the input on the screen
+const sendInputToDisplay = (number) => {
+    if(awaitsInput) {
+        display.textContent = number;
+        awaitsInput = false;
+    } else {
+        display.textContent = display.textContent === '0' ? number: display.textContent + number;
+    }
+}
+
+const clearScreen = () => {
+    awaitsInput = false;
+    firstValue = 0;
+    operator = '';
+    display.textContent = '0';
+}
+
+
+const useOperators = (operator) => {
+    if(!display.textContent) return;
+    if(awaitsInput) {
+        operatorValue = operator;
+        return;
+    };
+    console.log('did not returne')
+    const currentValue = Number(display.textContent);
+    console.log(currentValue)
+    if(!firstValue){
+        firstValue = currentValue;
+    } else {
+        console.log(operator);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        display.textContent = calculation;
+        console.log(currentValue, operatorValue, firstValue, '=', calculation);
+        firstValue = calculation;
+    }
+    awaitsInput = true;
+    operatorValue = operator;
+}
+
+
+const addDecimal = () => {
+    if(awaitsInput) return;
+    if(!display.textContent.includes('.')) sendInputToDisplay('.');
+}
+
+
+
+
+
+
+
+
+// adding event listeners to all the buttons
+inputBtns.forEach((inputBtn) => {
+    if(inputBtn.classList.length === 0){
+        inputBtn.addEventListener('click', () => sendInputToDisplay(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => useOperators(inputBtn.value));
+    }
+    else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click',addDecimal);
+    }
+})
+
+clearBtn.addEventListener('click', clearScreen);
